547dfba430a6b3dfb01dbd806e8948edf2ca7ff5
Thomas Sibley <trs@bestpractical.com>
2013-08-01 10:24:21 -0700

    Usage notes in the doc
    Based on a pull request by weizhou <weizhou@redhat.com> which edited the
    README directly instead of the POD.
    
    weizhou's text was rewritten slightly.

--- lib/RT/Extension/ReportSpam.pm
+++ lib/RT/Extension/ReportSpam.pm
@@ -6,6 +6,8 @@ use warnings;
 
 our $VERSION = '0.10';
 
+=encoding utf-8
+
 =head1 NAME
 
 RT::Extension::ReportSpam - mark tickets as spam with one click
@@ -15,24 +17,79 @@ RT::Extension::ReportSpam - mark tickets as spam with one click
 A simple extension that works with RT 4.0 and newer and allows users
 to report a ticket as a spam message.
 
-To find all tickets reported as spam you have to use query builder in
-advanced mode with the following query:
+=head1 INSTALLATION 
 
-    HasAttribute = 'SpamReports'
+=over
 
-=head1 RT 3.8
+=item C<perl Makefile.PL>
+
+=item C<make>
+
+=item C<make install>
+
+May need root permissions
+
+=item Edit your F</opt/rt4/etc/RT_SiteConfig.pm>
+
+Add this line:
+
+    Set(@Plugins, qw(RT::Extension::ReportSpam));
+
+or add C<RT::Extension::ReportSpam> to your existing C<@Plugins> line.
+
+=item Clear your mason cache
+
+    rm -rf /opt/rt4/var/mason_data/obj
+
+=item Restart your webserver
+
+=back
+
+=head1 USING ON RT 3.8
 
 Users of this extension on RT 3.8 should not upgrade past version 0.03 of this
 extension.
 
-=cut
+=head1 HOW TO …
 
-require RT::Tickets;
+=head2 Report Spam
 
+On any ticket page, you can report the ticket as spam by clicking "S".
+
+You can also click the "S" on the Tools → Spam → Recent page, which lists all
+the recently created tickets with status "new" and without owner.
+
+If you have rights to delete tickets you mark as spam, then they will be
+deleted automatically at the same time.
+
+=head2 Query All Tickets Reported as Spam
+
+To find all tickets reported as spam and not deleted you can navigate to Tools
+→ Spam → Reported, or use the query builder in advanced mode with the following
+query:
+
+    HasAttribute = 'SpamReports'
+
+Both of these methods list the tickets reported as spam by others without
+rights to delete tickets themselves.
+
+=head2 Confirm a Ticket as Spam
+
+You can confirm the reports by clicking "S" after querying all tickets reported
+as spam.  If you have rights to delete tickets then they will be deleted.
+
+=head1 BUGS
+
+All bugs should be reported via email to
+L<bug-RT-Extension-ReportSpam@rt.cpan.org|mailto:bug-RT-Extension-ReportSpam@rt.cpan.org>
+or via the web at
+L<rt.cpan.org|http://rt.cpan.org/Public/Dist/Display.html?Name=RT-Extension-ReportSpam>.
 
 =head1 AUTHOR
 
 Ruslan Zakirov E<lt>Ruslan.Zakirov@gmail.comE<gt>
+
+Thomas Sibley E<lt>trs@bestpractical.comE<gt>
 
 =head1 LICENSE
 
